c58a27340ba2855ea8063f7b98540008fbdd38c1
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-22 02:36:22 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,36 +1,29 @@
-RegisterServerEvent('oxydelivery:server')
-AddEventHandler('oxydelivery:server', function()
-	local player = QBCore.Functions.GetPlayer(source)
+QBCore = exports['qb-core']:GetCoreObject()
 
+RegisterNetEvent('oxydelivery:server', function()
+	local player = QBCore.Functions.GetPlayer(source)
 	if player.PlayerData.money['cash'] >= Config.StartOxyPayment then
 		player.Functions.RemoveMoney('cash', Config.StartOxyPayment)
-		
 		TriggerClientEvent("oxydelivery:startDealing", source)
 	else
 		TriggerClientEvent('QBCore:Notify', source, 'You dont have enough money', 'error')
 	end
 end)
 
-RegisterServerEvent('oxydelivery:receiveBigRewarditem')
-AddEventHandler('oxydelivery:receiveBigRewarditem', function()
+RegisterNetEvent('oxydelivery:receiveBigRewarditem', function()
 	local player = QBCore.Functions.GetPlayer(source)
-
 	player.PlayerData.AddItem(Config.BigRewarditem, 1)
 	TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[Config.BigRewarditem], "add")
 end)
 
-RegisterServerEvent('oxydelivery:receiveoxy')
-AddEventHandler('oxydelivery:receiveoxy', function()
+RegisterNetEvent('oxydelivery:receiveoxy', function()
 	local player = QBCore.Functions.GetPlayer(source)
-
 	player.Functions.AddMoney('cash', Config.Payment / 2)
 	player.Functions.AddItem(Config.Item, Config.OxyAmount)
 	TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[Config.Item], "add")
 end)
 
-RegisterServerEvent('oxydelivery:receivemoneyyy')
-AddEventHandler('oxydelivery:receivemoneyyy', function()
+RegisterNetEvent('oxydelivery:receivemoneyyy', function()
 	local player = QBCore.Functions.GetPlayer(source)
-
 	player.Functions.AddMoney('cash', Config.Payment)
 end)
